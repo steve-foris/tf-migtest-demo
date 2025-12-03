ec91070f0a3173bdfbfacdc8ea99e2381614333a
Steve Foris <20120769+steve-foris@users.noreply.github.com>
2025-12-02 19:18:40 -0800

    feat: output app_version as active instance template name to observe rolling updates

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,8 @@ output "lb_ip" {
   description = "External IP of HTTP load balancer"
   value       = google_compute_global_forwarding_rule.http.ip_address
 }
+
+output "app_version" {
+  description = "Active app version (instance template name based on active_color)"
+  value       = var.active_color == "green" ? google_compute_instance_template.green.name : google_compute_instance_template.blue.name
+}
